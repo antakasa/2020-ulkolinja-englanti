2604ffce75e664154a830c993565cf45e79bf41a
Antti Saarenpää <antti.saarenpaa@gmail.com>
2020-02-18 18:22:57 +0200

    .

--- src/components/finalPage.js
+++ src/components/finalPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './paragraphCover.css';
 import Pagination from './pagination';
-import BrandLogo from '../images/Ulkolinja_logo.png';
+import BrandLogo from '../images/Ulkolinja_logo.svg';
 import AreenaClip from './areenaClip';
 import ShareButtons from './shareButtons';
 import {data} from '../data';
